332a50c63361ea72e5a58bfc51228f8169042c0a
Anthony <ringeisen.anthony@icloud.com>
2025-06-24 19:14:40 +0200

    add function DECLARATION / EXPRESSION / ARROW

--- 01-Fundamentals-Part-2/functions/script.js
+++ 01-Fundamentals-Part-2/functions/script.js
@@ -17,3 +17,39 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(5, 2);
 console.log(appleOrangeJuice);
+
+
+// FUNCTION DECLARATION
+
+function calcAge1(birthYear){
+    return 2025 - birthYear;
+}
+
+const age1 = calcAge1(1996);
+console.log(age1)
+
+
+//FUNCTION EXPRESSION
+
+const calcAge2 = function(birthYear){
+    return 2025 - birthYear;
+}
+
+const age2 = calcAge2(1996);
+console.log(age2)
+
+// ARROW FUNCTIONS
+
+const calcAge3 = birthYear => 2025 - birthYear;
+const age3 = calcAge3(1996);
+console.log(age3);
+
+
+const yearUntilRetirement = (birthYear, firstName) => {
+    const age = 2025 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearUntilRetirement(1996, 'Sarah'));
+console.log(yearUntilRetirement(1990, 'John'));
