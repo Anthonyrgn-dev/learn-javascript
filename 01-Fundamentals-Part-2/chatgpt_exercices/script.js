48540f33a6068dd2aa16c827ae8cd0481c2d545c
Anthony <ringeisen.anthony@icloud.com>
2025-06-28 18:16:24 +0200

    mores exercises for function and finalize challenge

--- 01-Fundamentals-Part-2/chatgpt_exercices/script.js
+++ 01-Fundamentals-Part-2/chatgpt_exercices/script.js
@@ -42,3 +42,53 @@ const signeDuNombre = (x) => {
 }
 
 console.log(signeDuNombre(-5));
+
+// ---------------
+/*const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log( calcAverage(1, 2, 3));
+
+const studentA = calcAverage(15,12,13);
+const studentB = calcAverage(10,9,8);
+
+const checkResult = function (studentA, studentB){
+    if (studentA >= studentB + 5) {
+        console.log(`Student A wins the trophy! ${studentA} vs. ${studentB}`);
+    } else if (studentB >= studentA + 5) {
+        console.log(`Student B wins the trophy! ${studentB} vs. ${studentA}`);
+    } else {
+        console.log(`No one wins the trophy!!!!`);
+    }
+}
+
+console.log(checkResult(studentA, studentB)); */
+
+// ----------------
+
+'use strict';
+
+// 1. calcAverage : arrow function (3 scores → moyenne)
+
+const calcAverage = (a, b, c) => {
+    return (a + b + c) / 3;
+};
+
+// 2. Moyennes :
+const scoreLions = calcAverage(45, 39, 51);
+const scoreTigers = calcAverage(47, 50, 38);
+const scoreBears  = calcAverage(42, 42, 42);
+
+// 3. Fonction checkClassement
+
+const checkClassement = function (lions, tigers, bears) {
+    if (lions > tigers && lions > bears) {
+        console.log(`Lions win the trophy! ${scoreLions} vs. ${scoreTigers} vs. ${scoreBears}`);
+    } else if (tigers > lions && tigers > bears) {
+        console.log(`Tigers win the trophy! ${scoreTigers} vs. ${scoreLions} vs. ${scoreBears}`);
+    } else if (bears > lions && bears > tigers) {
+        console.log(`Bears win the trophy! ${scoreBears} vs. ${scoreLions} vs. ${scoreTigers}`);
+    } else if (lions === tigers || lions === bears) {
+        console.log(`No one wins the trophy!`);
+    }
+};
+checkClassement(scoreLions, scoreTigers, scoreBears);
+
--- 01-Fundamentals-Part-2/coding_challenge/challenge1/script.js
+++ 01-Fundamentals-Part-2/coding_challenge/challenge1/script.js
@@ -1,2 +1,22 @@
 'use strict';
 
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log( calcAverage(1, 2, 3));
+
+const scoreDolphins = calcAverage(44,23,71);
+const scoreKoalas = calcAverage(65, 54,49 );
+
+console.log(scoreDolphins);
+console.log(scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphins win the trophy! ${avgDolphins} vs. ${avgKoalas}`);
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        console.log(`Koalas win the trophy! ${avgKoalas} vs. ${avgDolphins}`);
+    } else {
+        console.log(`No one wins the trophy!`);
+    }
+}
+
+console.log(checkWinner(scoreDolphins, scoreKoalas));
