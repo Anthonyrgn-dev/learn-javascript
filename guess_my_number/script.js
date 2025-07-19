eee52b5a29741ada621e36ad6ea5f31544d0ac72
Anthony <ringeisen.anthony@icloud.com>
2025-07-19 16:14:48 +0200

    DOM section: Handling click event

--- guess_my_number/script.js
+++ guess_my_number/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// 1 SELECTING AND MANIPULATING DOM
+
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '👍 Correct Number !';
 
@@ -7,4 +10,19 @@ document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value = 23);
+console.log(document.querySelector('.guess').value = 23); */
+
+
+console.log(`========================`);
+
+// 2 HANDLING CLICK EVENT
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof  guess);
+
+    if(!guess) {
+        document.querySelector('.message').textContent = "🚫 Please enter a valid number";
+    }
+
+})
